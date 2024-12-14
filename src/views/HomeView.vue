b2c59c7a7fe6c4be95f811e6b78b488e6f129e90
Poirrier Peter <peterdesete@gmail.com>
2024-12-14 14:54:08 +0100

    feat: Ajout animation de background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { FwbJumbotron } from 'flowbite-vue'
+import StarsBackground from '@/components/StarsBackground.vue'
 </script>
 
 <template>
   <div>
     <fwb-jumbotron header-text="Join an engaged community now!"
-                   sub-text="Organise E-sport-focused event with ease.">
+                   sub-text="Organise E-sport-focused event with ease."
+                   style="background: transparent;">
       <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
         <a href="#"
            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -13,5 +15,6 @@ import { FwbJumbotron } from 'flowbite-vue'
         </a>
       </div>
     </fwb-jumbotron>
+    <stars-background></stars-background>
   </div>
 </template>
