62cf0bd8b5e51bb9b5ab386c91e5e9c9364f399a
tueurDeDragon <ramnikhil45@gmail.com>
2024-12-14 14:51:40 +0100

    feat(add des interfaces):
    (garaged donc je push pour que vous pussiez travailler, je les corrigerai à mon retour en cas de problème)

---
+++ src/type.ts
@@ -0,0 +1,95 @@
+export interface User {
+  id: number;
+  members: Member[];
+  customerRegistrations: CustomerRegistration[];
+  managers: Manager[];
+  createdRewards: Reward[];
+  createdEvents: Event[];
+  createdTeams: Team[];
+}
+
+export interface Sponsor {
+  id: number;
+  eventSponsors: EventSponsor[];
+  teamSponsors: TeamSponsor[];
+}
+
+export interface Event {
+  id: number;
+  eventRewards: EventReward[];
+  eventSponsors: EventSponsor[];
+  customerRegistrations: CustomerRegistration[];
+  confrontations: Confrontation[];
+  managers: Manager[];
+  teamRegistrations: TeamRegistration[];
+}
+
+export interface Reward {
+  id: number;
+  prizePacks: PrizePack[];
+}
+
+export interface EventReward {
+  id: number;
+  prizePacks: PrizePack[];
+  recipients: Recipient[];
+}
+
+export interface TeamSponsor {
+  id: number;
+}
+
+export interface Manager {
+  id: number;
+}
+
+export interface Confrontation {
+  id: number;
+  participations: Participation[];
+}
+
+export interface CustomerRegistration {
+  id: number;
+}
+
+export interface EventSponsor {
+  id: number;
+}
+
+export interface Game {
+  id: number;
+  confrontations: Confrontation[];
+}
+
+export interface Participation {
+  id: number;
+}
+
+export interface PrizePack {
+  id: number;
+}
+
+export interface Recipient {
+  id: number;
+}
+
+export interface RefreshToken {
+  id: number;
+}
+
+export interface Member {
+  id: number;
+}
+
+export interface Team {
+  id: number;
+  recipients: Recipient[];
+  teamSponsors: TeamSponsor[];
+  members: Member[];
+  participations: Participation[];
+  teamRegistrations: TeamRegistration[];
+}
+
+export interface TeamRegistration {
+  id: number;
+}
